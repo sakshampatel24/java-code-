ee071230271b73d2178790ae5d78a44449ae2dd8
Sakshampatel <134845764+saksr@users.noreply.github.com>
2024-02-20 16:52:03 +0530

    Add files via upload

---
+++ day1/src/day1/Firstclass.java
@@ -0,0 +1,13 @@
+package day1;
+public class Firstclass {
+	public static void main(String[] args) {
+		int a = 10,b = 20;
+		a = a+b;
+		b = a-b;
+		a = a-b;
+		System.out.println(a);
+		System.out.println(b);
+    }
+}
+
+
---
+++ day1/src/day1/Secondday.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class Secondday {
+	    public static void main(String[] args) {
+	         double number1 = 10;
+	         double number2 = 20;
+	         double number3 = 15;
+
+	        if (number1 >= number2 && number1 >= number3) {
+	            System.out.println("The biggest number is: " + number1);
+	        } else if (number2 >= number1 && number2 >= number3) {
+	            System.out.println("The biggest number is: " + number2);
+	        } else {
+	            System.out.println("The biggest number is: " + number3);
+	        }
+	    }
+	}
+	    
+
+
