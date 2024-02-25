61285e8329a6cfd41a7690ea221edd229a05ee81
Saksham patel <134845764+saksr@users.noreply.github.com>
2024-02-25 10:49:39 +0530

    Add files via upload

---
+++ employee/Employee.java
@@ -0,0 +1,15 @@
+package employee;
+
+public class Employee {
+	
+	private String ename,email,designation;
+	public void getdata() {
+		ename ="Saksham";
+		email="patelska@223gamil.com";
+		designation="Software engineer";
+	}
+	public void display() {
+		System.out.println(ename +" " +email+" "+" "+designation);
+	}
+
+}
---
+++ employee/Employeedemo.java
@@ -0,0 +1,13 @@
+package employee;
+
+public class Employeedemo {
+
+	public static void main(String[] args) {
+	
+		Employee emp1 = new Employee();
+		emp1.getdata();
+		emp1.display();
+
+	}
+
+}
