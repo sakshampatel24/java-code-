72939dea7c7395253fee1e50001368cf65e05529
Saksham patel <134845764+sakshampatel24@users.noreply.github.com>
2024-03-19 22:03:04 +0530

    Add files via upload

---
+++ string/Main.java
@@ -0,0 +1,12 @@
+package string;
+//Character of string by changing its case//
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        char firstChar = str.charAt(0);
+        char lowerCaseFirstChar = Character.toLowerCase(firstChar);
+        char upperCaseFirstChar = Character.toUpperCase(firstChar);
+        System.out.println("First Character in lowercase: " + lowerCaseFirstChar);
+        System.out.println("First Character in uppercase: " + upperCaseFirstChar);
+    }
+}
