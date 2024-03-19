0b01db9bd89e9be204970b6fcc42d99c6a23ef67
Saksham patel <134845764+sakshampatel24@users.noreply.github.com>
2024-03-19 22:01:36 +0530

    Add files via upload

---
+++ student/Studentdemo.java
@@ -0,0 +1,12 @@
+package student;
+
+public class Studentdemo {
+	    public static void main(String[] args) {
+	        Student student1 = new Student("jay", 20, "456");
+	        System.out.println("Student Name: " + student1.getName());
+	        System.out.println("Student Age: " + student1.getAge());
+	        System.out.println("Student ID: " + student1.getId());
+	       
+	    }
+	}
+
